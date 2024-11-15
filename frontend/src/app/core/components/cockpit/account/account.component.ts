3c7d3dc5411f2b3b027849020a581a47c5ad8e3f
Rohullah <rohullahniazi03@gmail.com>
2024-11-16 04:04:22 +0900

    Fixed profile component

--- frontend/src/app/core/components/cockpit/account/account.component.ts
+++ frontend/src/app/core/components/cockpit/account/account.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,input,Input } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
-  account = {
-    name: 'Robert',
-    posts: 10,
-    followers: 2,
-    following: 180,
-    bio: 'Just enjoying my life!',
-    avatar: 'assets/accounts/robertIcon.png', 
-    postImage: 'assets/accounts/robertPosts.png' 
+  @Input() account: any = {
+    name: '',
+    posts: 0,
+    followers: 0,
+    following: 0,
+    bio: '',
+    avatar: '',
+    fText: '',
+    folowersImage: [],
+    postImage: []
   };
 
 }
--- frontend/src/app/features/phone/phone.component.ts
+++ frontend/src/app/features/phone/phone.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AccountComponent } from "../../core/components/cockpit/account/account.component";
+import { AccountComponent } from "../../core/components/cockpit/account/account.component"; 
+
 
 @Component({
   selector: 'app-phone',
@@ -9,5 +10,58 @@ import { AccountComponent } from "../../core/components/cockpit/account/account.
   styleUrl: './phone.component.scss'
 })
 export class PhoneComponent {
+  public AccountArray = [{
+    name: 'Robert',
+    posts: 10,
+    followers: 2,
+    following: 180,
+    bio: 'Just enjoying my life!',
+    avatar: 'assets/accounts/robertIcon.png',
+    fText: "Gefolgt von paulfriedrich, juliaowczarek und 92 weiteren",
+    folowersImage: ['assets/accounts/robertIcon.png'],
+    postImage: ['assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png', 'assets/accounts/robertIcon.png']
+}, {
+    name: 'Robert',
+    posts: 10,
+    followers: 200,
+    following: 180,
+    bio: 'Just enjoying my life!',
+    avatar: 'assets/accounts/robertIcon.png',
+    fText: "Gefolgt von paulfriedrich, juliaowczarek und 92 weiteren",
+    folowersImage: [
+        'assets/followers/follower1.png',
+        'assets/followers/follower2.png',
+        'assets/followers/follower3.png'
+    ],
+    postImage: []
+}, {
+    name: 'Robert',
+    posts: 10,
+    followers: 2,
+    following: 180,
+    bio: 'Just enjoying my life!',
+    avatar: 'assets/accounts/robertIcon.png',
+    fText: "Gefolgt von paulfriedrich, juliaowczarek und 92 weiteren",
+    folowersImage: [
+        'assets/followers/follower1.png',
+        'assets/followers/follower2.png',
+        'assets/followers/follower3.png'
+    ],
+    postImage: []
+}, {
+    name: 'Robert',
+    posts: 10,
+    followers: 2,
+    following: 180,
+    bio: 'Just enjoying my life!',
+    avatar: 'assets/accounts/robertIcon.png',
+    fText: "Gefolgt von paulfriedrich, juliaowczarek und 92 weiteren",
+    folowersImage: [
+        'assets/followers/follower1.png',
+        'assets/followers/follower2.png',
+        'assets/followers/follower3.png'
+    ],
+    postImage: []
+},]
 
 }
