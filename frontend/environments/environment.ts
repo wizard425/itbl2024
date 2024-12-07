c304c06fe17c09f901ac5df90ca1a489141be4c6
SAMUEL\samue <samuel.kob03@gmail.com>
2024-12-07 20:46:19 +0100

    changed port for backend-expose and urls

--- frontend/environments/environment.ts
+++ frontend/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    baseUrl: "http://localhost:5000/api/",
+    //baseUrl: "http://localhost:5000/api/",
+    baseUrl: "http://ample-imagination.railway.internal/api/",
     storeGameStep: false,
     storeUser: true
 }
