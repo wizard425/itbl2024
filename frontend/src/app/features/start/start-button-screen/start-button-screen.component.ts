c03d6d26b047166a9310d91cfc7529354cc1596c
Rohullah <rohullahniazi03@gmail.com>
2025-03-26 03:20:17 +0900

    consent is added to the start page

--- frontend/src/app/features/start/start-button-screen/start-button-screen.component.ts
+++ frontend/src/app/features/start/start-button-screen/start-button-screen.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { ConsentDialogComponent } from "../../../core/components/consent/consent-dialog/consent-dialog.component";
 
 @Component({
   selector: 'app-start-button-screen',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatButtonModule, MatIconModule, RouterModule, ConsentDialogComponent],
   templateUrl: './start-button-screen.component.html',
   styleUrl: './start-button-screen.component.scss'
 })
